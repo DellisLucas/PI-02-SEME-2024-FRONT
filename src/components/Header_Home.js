49e5ae92ef23d42f4931b521d1a22adc1079d966
leRangel18 <leticiacamargomarmo706@gmail.com>
2024-12-03 12:06:21 -0300

    Adicionando botão de sair do sistema

--- src/components/Header_Home.js
+++ src/components/Header_Home.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./../styles/Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
 const Header_Home = () => {
   const [isReportModalOpen, setIsReportModalOpen] = useState(false); // Controle do modal
@@ -45,10 +47,15 @@ const Header_Home = () => {
     }
   };
 
+  const handleLogout = () => {
+    // Remove o token do localStorage e redireciona para a tela de login
+    localStorage.removeItem("token");
+    window.location.href = "/login";
+  };
+
   return (
     <>
       <header className="header">
-        
         <div className="logo-container">
           <img src="/img/icon-logo.png" alt="Logo Icon" className="logo-icon" />
           <img src="/img/cashtab.png" alt="Logo Text" className="logo-name" />
@@ -66,77 +73,80 @@ const Header_Home = () => {
           >
             Relatórios
           </a>
+          <button className="logout-button" onClick={handleLogout}>
+            <FontAwesomeIcon icon={faSignOutAlt} />
+          </button>
         </nav>
       </header>
       {isReportModalOpen && (
-  <div className="report-modal">
-    <div className="report-modal-content">
-      <h2>Gerar Relatório</h2>
-      <form>
-        <label>Tipo de Relatório</label>
-        <select
-          value={filters.type}
-          onChange={(e) =>
-            setFilters({ ...filters, type: e.target.value })
-          }
-        >
-          <option value="expenses">Despesas</option>
-          <option value="incomes">Receitas</option>
-          <option value="both">Ambos</option>
-        </select>
+        <div className="report-modal">
+          <div className="report-modal-content">
+            <h2>Gerar Relatório</h2>
+            <form>
+              <label>Tipo de Relatório</label>
+              <select
+                value={filters.type}
+                onChange={(e) =>
+                  setFilters({ ...filters, type: e.target.value })
+                }
+              >
+                <option value="expenses">Despesas</option>
+                <option value="incomes">Receitas</option>
+                <option value="both">Ambos</option>
+              </select>
 
-        <label>Período</label>
-        <input
-          type="date"
-          value={filters.startDate}
-          onChange={(e) =>
-            setFilters({ ...filters, startDate: e.target.value })
-          }
-        />
-        <input
-          type="date"
-          value={filters.endDate}
-          onChange={(e) =>
-            setFilters({ ...filters, endDate: e.target.value })
-          }
-        />
+              <label>Período</label>
+              <input
+                type="date"
+                value={filters.startDate}
+                onChange={(e) =>
+                  setFilters({ ...filters, startDate: e.target.value })
+                }
+              />
+              <input
+                type="date"
+                value={filters.endDate}
+                onChange={(e) =>
+                  setFilters({ ...filters, endDate: e.target.value })
+                }
+              />
 
-        <label>Filtrar por Valor</label>
-        <input
-          type="number"
-          placeholder="Valor mínimo"
-          value={filters.minValue}
-          onChange={(e) =>
-            setFilters({ ...filters, minValue: e.target.value })
-          }
-        />
-        <input
-          type="number"
-          placeholder="Valor máximo"
-          value={filters.maxValue}
-          onChange={(e) =>
-            setFilters({ ...filters, maxValue: e.target.value })
-          }
-        />
+              <label>Filtrar por Valor</label>
+              <input
+                type="number"
+                placeholder="Valor mínimo"
+                value={filters.minValue}
+                onChange={(e) =>
+                  setFilters({ ...filters, minValue: e.target.value })
+                }
+              />
+              <input
+                type="number"
+                placeholder="Valor máximo"
+                value={filters.maxValue}
+                onChange={(e) =>
+                  setFilters({ ...filters, maxValue: e.target.value })
+                }
+              />
 
-        <div className="report-modal-actions">
-          <button
-            type="button"
-            onClick={handleGenerateReport}
-          >
-            Gerar Relatório
-          </button>
-          <button
-            type="button"
-            onClick={() => setIsReportModalOpen(false)} // Fecha o modal
-          >
-            Fechar
-          </button>
+              <div className="report-modal-actions">
+                <button
+                  type="button"
+                  onClick={handleGenerateReport}
+                >
+                  Gerar Relatório
+                </button>
+                <button
+                  type="button"
+                  onClick={() => setIsReportModalOpen(false)} // Fecha o modal
+                >
+                  Fechar
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-      </form>
-    </div>
-  </div>
-)}
+      )}
     </>
   );
 };
